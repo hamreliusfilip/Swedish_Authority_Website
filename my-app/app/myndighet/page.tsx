1cad47e5c0cd6546690cea4411d20c85a9757ec6
filha243 <filha243@student.liu.se>
2024-04-03 11:51:23 +0200

    Nested routing for myndigheter page

--- my-app/app/myndighet/page.tsx
+++ my-app/app/myndighet/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import CompleteMenu from '../../components/completeMenu';
 import Logo from '../../components/logo';
+import Link from 'next/link';
 
 export default function Page() {
     return (
         <div>
             <Logo />
             <CompleteMenu />
-            <h1>Myndighet</h1>
+            <Link href="/myndighet/listaMyndighet"> Klicka för sida för myndigheter lista </Link> <br />
+            <Link href="/myndighet/specifikMyndighet"> Klicka för sida för specifik myndighet </Link>
         </div>
     );
 }
