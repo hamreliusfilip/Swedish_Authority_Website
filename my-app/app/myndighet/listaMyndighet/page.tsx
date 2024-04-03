5d9b8c81041d82f719b19896184a2891a61cb06f
Anna Granberg <annaagranberg@gmail.com>
2024-04-03 15:46:47 +0200

    Förta utkast av mongoDB

--- my-app/app/myndighet/listaMyndighet/page.tsx
+++ my-app/app/myndighet/listaMyndighet/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import CompleteMenu from '../../../components/completeMenu';
 import Logo from '../../../components/logo';
 
+
+
 export default function Page() {
     return (
         <div>
