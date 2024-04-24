ae6a920a952fa8de989396081546f7b48aa9e8dc
filha243 <filha243@student.liu.se>
2024-04-24 13:39:22 +0200

    Filtering done

--- my-app/app/myndighet/page.tsx
+++ my-app/app/myndighet/page.tsx
@@ -18,6 +18,7 @@ import {
     AccordionTrigger,
 } from "@/components/ui/accordion"
 import { Button } from '@/components/ui/button';
+import { Slider } from '@/components/ui/slider';
 
 import { Myndigheter } from '@/lib/models/myndighet';
 import CompleteMenu from '../../components/Main/completeMenu';
@@ -34,6 +35,9 @@ export default function Page() {
     const [ruleFilters, setRuleFilters] = useState<Record<string, boolean>>({});
     const [relationFilters, setRelationFilters] = useState<Record<string, boolean>>({});
     const [filterReset, setFilterReset] = useState(false);
+    const [filteredMyndigheter, setFilteredMyndigheter] = useState<Myndigheter[]>([]);
+    const [slider1Value, setSlider1Value] = useState('1623');
+    const [slider2Value, setSlider2Value] = useState('2024');
 
     const fetchMyndigheter = async () => {
         try {
@@ -69,26 +73,40 @@ export default function Page() {
         setRuleFilters(filters);
     };
 
-    const filteredMyndigheter = myndigheter.filter(myndighet => {
-
-        const nameMatch = myndighet.name.toLowerCase().includes(searchQuery.toLowerCase());
-        const orgMatch = myndighet.org.toLowerCase().includes(searchQuery.toLowerCase());
-
-        const relationMatch = Object.entries(relationFilters).every(([relation, checked]) => !checked || myndighet.relation === relation);
-        const ruleMatch = Object.entries(ruleFilters).every(([rule, checked]) => !checked || myndighet.rule === rule);
-
-        return (nameMatch || orgMatch) && ruleMatch && relationMatch;
-    });
+    useEffect(() => {
+        const filteredMyndigheter = myndigheter.filter(myndighet => {
+            const nameMatch = myndighet.name.toLowerCase().includes(searchQuery.toLowerCase());
+            const orgMatch = myndighet.org.toLowerCase().includes(searchQuery.toLowerCase());
+            const minValue = parseInt(slider1Value);
+            const maxValue = parseInt(slider2Value);
+            const createdMatch = myndighet.created >= minValue && myndighet.created <= maxValue;
+            const relationMatch = Object.entries(relationFilters).every(([relation, checked]) => !checked || myndighet.relation === relation);
+            const ruleMatch = Object.entries(ruleFilters).every(([rule, checked]) => !checked || myndighet.rule === rule);
+            return (nameMatch || orgMatch) && ruleMatch && relationMatch && createdMatch;
+        });
+        setFilteredMyndigheter(filteredMyndigheter);
+    }, [myndigheter, searchQuery, slider1Value, slider2Value, relationFilters, ruleFilters]);
 
     const handleClearFilters = () => {
         setRuleFilters({});
         setRelationFilters({});
         setSearchQuery('');
         setFilterReset(true);
+        setSlider1Value('1623');
+        setSlider2Value('2024');
 
         setTimeout(() => {
             setFilterReset(false);
         }, 0);
+    };
+
+    // Slider 
+    const handleInput1Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSlider1Value(event.target.value);
+    };
+
+    const handleInput2Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSlider2Value(event.target.value);
     };
 
     return (
@@ -98,7 +116,6 @@ export default function Page() {
                 <CompleteMenu />
             </div>
             <div className='flex flex-column m-4 gap-5 justify-items-center h-30'>
-
                 <div className='basis-1/3 ml-10'>
                     <Card className='p-5'>
                         <CardTitle className='mb-5'>Filtreringsalternativ</CardTitle>
@@ -115,7 +132,6 @@ export default function Page() {
                                     </AccordionContent>
                                 </AccordionItem>
                             </Accordion>
-
                             <Accordion type="single" collapsible>
                                 <AccordionItem value="item-1">
                                     <AccordionTrigger>Styre</AccordionTrigger>
@@ -125,6 +141,37 @@ export default function Page() {
                                             onChange={handleRuleFilterChange}
                                             reset={filterReset}
                                         />
+                                    </AccordionContent>
+                                </AccordionItem>
+                            </Accordion>
+                            <Accordion type="single" collapsible>
+                                <AccordionItem value="item-1">
+                                    <AccordionTrigger>Årtal</AccordionTrigger>
+                                    <AccordionContent>
+                                        <div className='flex items-center mt-3'>
+                                            <div className='flex items-center text-sm font-semibold'>
+                                                <p className='mr-4'>Från</p>
+                                                <input
+                                                    type="number"
+                                                    min="1623"
+                                                    max="1851"
+                                                    value={slider1Value}
+                                                    onChange={handleInput1Change}
+                                                    className=' border border-bg-slate-300 rounded p-1'
+                                                />
+                                            </div>
+                                            <div className='flex items-center ml-7 text-sm font-semibold'>
+                                                <p className='mr-4 font-semibold text-sm'>Till&nbsp;&nbsp;</p>
+                                                <input
+                                                    type="number"
+                                                    min="1851"
+                                                    max="2024"
+                                                    value={slider2Value}
+                                                    onChange={handleInput2Change}
+                                                    className=' border border-bg-slate-300 rounded p-1'
+                                                />
+                                            </div>
+                                        </div>
                                     </AccordionContent>
                                 </AccordionItem>
                             </Accordion>
@@ -140,7 +187,8 @@ export default function Page() {
                         onChange={handleSearchChange}
                         className="border border-gray-200 rounded-md p-2 w-full mb-4 font-light font-sans text-sm"
                     />
-                    <div className='overflow-y-auto'>
+                    <p className='font-slate-300 text-sm font-light'>Antal hittade myndigheter: {filteredMyndigheter.length} st</p>
+                    <div className='overflow-y-auto mt-4'>
                         <Card>
                             {filteredMyndigheter.map((myndighet: any) => (
                                 <div key={myndighet._id}>
