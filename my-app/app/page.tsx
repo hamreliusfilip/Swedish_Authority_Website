1285d68b8b94cee1c12898278dee7b3f842eee28
Anna Granberg <annaagranberg@gmail.com>
2024-05-03 09:37:21 +0200

    to pull

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import CompleteMenu from '../components/Main/completeMenu';
 import Logo from '../components/Main/logo';
-import Info from '../components/Other/infoText';
 import NavCardsHoms from '../components/LandingPage/navCardsHoms';
 import Footer from '../components/Main/footer';
 import LandingFiller from '../components/LandingPage/landingFiller';
 import LandingFiller2 from "@/components/LandingPage/landingFiller2";
-import Facts from "@/components/Other/facts";
+import Info from "../components/Other/infoText";
+import Facts from "@/components/LandingPage/facts";
 
 export default function Home() {
 
